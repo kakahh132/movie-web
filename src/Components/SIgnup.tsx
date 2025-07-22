ba4719e0d773804f085fed189d23ebbc044dfc71
kakahh132 <kakachimka33@gmail.com>
2025-07-22 05:27:27 +0100

    second commit

--- src/Components/SIgnup.tsx
+++ src/Components/SIgnup.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import type { FormEvent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
-import './SignUp.css';
+import './signUp.css';
 
 type SignUpProps = {
   // Props can be added here if needed
