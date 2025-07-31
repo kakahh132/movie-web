bd270799d1fd0cb6a1f1314fbc1258d1aabaac2f
kakahh132 <kakachimka33@gmail.com>
2025-07-31 12:27:37 +0100

    first entry

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import "./App.css";
 import Watchlist from "./Components/Watchlist";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
-import SignUp from "./Components/Signup";
 import Hero from "./Components/Hero";
 function App() {
   return (
@@ -13,8 +12,9 @@ function App() {
      
        
       <Routes>
+        
          <Route path="/Footer" element={<Footer />} />
-          <Route path="/Signup" element={<SignUp />} />
+          
         <Route path="/Search" element={<Search />} />
         <Route path="/Watchlist" element={<Watchlist/>} />
         <Route path="/Home" element={<Home/>} />
